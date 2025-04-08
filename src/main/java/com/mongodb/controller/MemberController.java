3723df1f19c0a8ed5c42040a312644309b6476f4
Mayank Gupta <mayank.4.gupta@coforge.com>
2025-04-08 18:07:05 +0530

    Added CORS policy

--- src/main/java/com/mongodb/controller/MemberController.java
+++ src/main/java/com/mongodb/controller/MemberController.java
@@ -8,11 +8,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,6 +23,9 @@ import com.mongodb.service.MemberService;
 
 import jakarta.validation.Valid;
 
+@CrossOrigin(origins = "http://localhost:3000", allowedHeaders = "*", methods = { RequestMethod.GET, RequestMethod.POST,
+		RequestMethod.OPTIONS,
+		RequestMethod.HEAD }, allowCredentials = "true")
 @RestController
 @Validated
 @RequestMapping("/members")
@@ -30,7 +35,7 @@ public class MemberController {
 	private MemberService memberService;
 
 	// Get member by ID
-	@GetMapping("/{id:[0-9]+}")
+	@GetMapping("/{id}")
 	public ResponseEntity<MemberDto> getMemberById(@PathVariable("id") String id) {
 		Optional<MemberDto> member = memberService.findById(id);
 		if (member.isEmpty()) {
